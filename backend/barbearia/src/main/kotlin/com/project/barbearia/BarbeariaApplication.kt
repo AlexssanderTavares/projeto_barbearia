b30e8e2c01083c283c3e784046f0090e2f5d6cf3
Alexssander Tavares <alexssander.tavares.dev@gmail.com>
2025-11-14 17:31:20 -0300

    First commit

---
+++ backend/barbearia/src/main/kotlin/com/project/barbearia/BarbeariaApplication.kt
@@ -0,0 +1,11 @@
+package com.project.barbearia
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BarbeariaApplication
+
+fun main(args: Array<String>) {
+	runApplication<BarbeariaApplication>(*args)
+}
