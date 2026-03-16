d453db4521ba7610caf4661067b901119eb08d3a
Matheus Bressane <devmatybressane@gmail.com>
2026-03-15 21:42:21 -0300

    chore: add frontend base structure with Next.js

--- frontend/app/teste2.jsx
+++
@@ -1,7 +0,0 @@
-export default function Teste2 () {
-    return (
-        <div className="bg-blue-800 flex flex-col items-center justify-center h-5 w-5">
-            <h2>=</h2>
-        </div>
-    )
-}
